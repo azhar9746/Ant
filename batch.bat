4d976b110b5edc73ab821146a7f28f22d285330d
azhar9746 <42645291+azhar9746@users.noreply.github.com>
2019-01-14 17:29:21 +0530

    Delete batch.bat

--- batch.bat
+++
@@ -1,5 +0,0 @@
-set projectLocation=D:\U61976\eclipse-workspace\ANTProject
-cd %projectLocation%
-set classpath=%projectLocation%\bin;%projectLocation%\lib\*
-java org.testng.TestNG %projectLocation%\run.xml
-pause
